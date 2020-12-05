eb0dc17a1c4f74c9a71185e21928b7704db9efcb
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-05 11:51:23 -0600

    usda map, make responsive later

--- src/views/Home.js
+++ src/views/Home.js
@@ -13,6 +13,9 @@ export default function Home({ user }) {
   return (
     <div>
       <h1>Plant-It</h1>
+
+      <img className="homePageImg" src="https://lh3.googleusercontent.com/proxy/IkD6-juX6wRRnNm2g-KILFM45mH88x7Ua9sg5-2SH74kITPRukL6HwKw2WWjK1HWoa_AzwqbJw6y5wmHUm8A6MSRRKKeTQDQhmLWcKjPIs0uueQ2TmXUXn27eQAdHw" alt="usda map" width="1100" height="700"/>
+
       {loadComponent()}
     </div>
   );
