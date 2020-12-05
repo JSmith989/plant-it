0017220277f7c4db0ce6d0bc2dc24aef9b89ba37
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-05 00:30:06 -0600

    auth and routes set up

---
+++ src/components/TheNavbar/index.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
+export default function TheNavbar(props) {
+  const logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  };
+  const { user } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div>
+      <Navbar color='dark' dark expand='md' className='justify-content-between'>
+        <Link className="navbar-brand" to='/'>Plant-It</Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='mr-auto' navbar>
+            <NavItem>
+              <Link className="nav-link" to='/plants'>Plants</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to='/gardens'>
+                Gardens
+              </Link>
+            </NavItem>
+          </Nav>
+          {
+            user
+            && <>
+              <img className="userInfo" src={user?.photoURL} alt={user?.displayName} />
+              <UncontrolledDropdown>
+              <DropdownToggle nav caret>
+              </DropdownToggle>
+              <DropdownMenu right>
+              <DropdownItem>
+                {user?.displayName}
+                </DropdownItem>
+                <DropdownItem>
+                  <div
+                    className='nav-link btn btn-danger'
+                    onClick={(e) => logMeOut(e)}
+                  >
+                    Logout
+                  </div>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            </>
+          }
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
---
+++ src/views/Gardens.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Gardens extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Gardens</h1>
+      </div>
+    );
+  }
+}
