56f0ece5572fbf9c22042c60b448c4c5a6852c77
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-06 23:28:57 -0600

    garden reads but not carousel

--- src/views/Gardens.js
+++ src/views/Gardens.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { getAllGardens } from '../helpers/data/gardenData';
+import GardensCard from '../components/Cards/GardensCard';
+import Carousel from '../components/Carousel';
 
 export default class Gardens extends Component {
+  state = {
+    gardens: [],
+  }
+
+  componentDidMount() {
+    this.getGardens();
+  }
+
+  getGardens = () => {
+    getAllGardens().then((response) => {
+      this.setState({
+        gardens: response,
+      });
+    });
+  }
+
   render() {
+    const { gardens } = this.state;
+    const showGardens = () => (
+      gardens.map((garden) => <GardensCard key={garden.firebaseKey} garden={garden} />)
+    );
     return (
       <div>
         <h1 className="text-white">Gardens</h1>
+        <Carousel>
+          {showGardens()}
+        </Carousel>
       </div>
     );
   }
