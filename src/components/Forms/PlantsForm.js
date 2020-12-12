98bd010cd9c76e23d1cbf1afdc24d40488297e28
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-11 22:53:43 -0600

    adding fbkey but no other data

---
+++ src/components/Forms/PlantsForm.js
@@ -0,0 +1,234 @@
+import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import {
+  Button, Form, FormGroup, Label, Input,
+} from 'reactstrap';
+import {
+  createPlant,
+  createGardenPlant,
+  deleteKeyValue,
+  updatePlant,
+} from '../../helpers/data/plantData';
+import getUid from '../../helpers/data/authData';
+import GardenDropdown from '../GardenDropdown';
+// import { getUsersGardens } from '../../helpers/data/gardenData';
+
+export default class PlantsForm extends Component {
+  state = {
+    depth: this.props.plant?.depth || '',
+    firebaseKey: this.props.plant?.firebaseKey || '',
+    germination: this.props.plant?.germination || '',
+    harvesting: this.props.plant?.harvesting || '',
+    image: this.props.plant?.image || '',
+    name: this.props.plant?.name || '',
+    notes: this.props.plant?.notes || '',
+    spacing: this.props.plant?.spacing || '',
+    userId: this.props.plant?.userId || '',
+    zone: this.props.plant?.zone || '',
+    gardenId: this.props.plant?.gardenId || '',
+  };
+
+  componentDidMount() {
+    const userId = getUid();
+    this.setState({
+      userId,
+    });
+  }
+
+  //   getGardens = () => {
+  //     const currentUid = getUid();
+  //     getUsersGardens(currentUid).then((response) => {
+  //       this.setState({ gardens: response });
+  //     });
+  //   }
+
+  handleChange = (e) => {
+    if (e.target.name === 'filename') {
+      this.setState({ image: '' });
+
+      const storageRef = firebase.storage().ref();
+      const imageRef = storageRef.child(`plant-it/${this.state.userId}/${Date.now()}${e.target.files[0].name}`);
+
+      imageRef.put(e.target.files[0]).then((snapshot) => {
+        snapshot.ref.getDownloadURL().then((image) => {
+          this.setState({ image });
+        });
+      });
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.firebaseKey === '') {
+      const newPlant = {
+        depth: this.props.plant?.depth || '',
+        firebaseKey: this.props.plant?.firebaseKey || '',
+        germination: this.props.plant?.germination || '',
+        harvesting: this.props.plant?.harvesting || '',
+        image: this.props.plant?.image || '',
+        name: this.props.plant?.name || '',
+        notes: this.props.plant?.notes || '',
+        spacing: this.props.plant?.spacing || '',
+        userId: this.props.plant?.userId || '',
+        zone: this.props.plant?.zone || '',
+      };
+      createPlant(newPlant)
+        .then((response) => {
+          const joinTableObject = {
+            gardenId: this.state.gardenId,
+            plantId: response.data.firebaseKey,
+            userId: this.state.userId,
+          };
+          createGardenPlant(joinTableObject);
+        }).then(() => {
+          this.props.onUpdate(this.props.gardenId);
+        });
+    } else {
+      const plantUpdate = {
+        depth: this.props.plant?.depth || '',
+        firebaseKey: this.props.plant?.firebaseKey || '',
+        germination: this.props.plant?.germination || '',
+        harvesting: this.props.plant?.harvesting || '',
+        image: this.props.plant?.image || '',
+        name: this.props.plant?.name || '',
+        notes: this.props.plant?.notes || '',
+        spacing: this.props.plant?.spacing || '',
+        userId: this.props.plant?.userId || '',
+        zone: this.props.plant?.zone || '',
+      };
+      deleteKeyValue(this.state.firebaseKey);
+      updatePlant(plantUpdate)
+        .then(() => {
+          const updatedTable = {
+            gardenId: this.state.gardenId,
+            plantId: this.state.firebaseKey,
+            userId: this.state.userId,
+          };
+          createGardenPlant(updatedTable);
+          this.props.onUpdate(this.props.plant.firebaseKey);
+        });
+    }
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+          <GardenDropdown onChange={this.handleChange} />
+        <FormGroup>
+          <Label for='plantName'>Name</Label>
+          <Input
+            type='text'
+            name='name'
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder='Plant Name'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='plantZone'>Zone</Label>
+          <Input
+            type='text'
+            name='zone'
+            value={this.state.zone}
+            onChange={this.handleChange}
+            placeholder='Plant Zone'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='germination'>Germination</Label>
+          <Input
+            type='text'
+            name='germination'
+            value={this.state.germination}
+            onChange={this.handleChange}
+            placeholder='Germination'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='spacing'>Spacing</Label>
+          <Input
+            type='text'
+            name='spacing'
+            value={this.state.spacing}
+            onChange={this.handleChange}
+            placeholder='Spacing'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='depth'>Depth</Label>
+          <Input
+            type='text'
+            name='depth'
+            value={this.state.depth}
+            onChange={this.handleChange}
+            placeholder='Depth'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='notes'>Notes</Label>
+          <Input
+            type='text'
+            name='notes'
+            value={this.state.notes}
+            onChange={this.handleChange}
+            placeholder='Notes'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='harvesting'>Harvesting</Label>
+          <Input
+            type='text'
+            name='harvesting'
+            value={this.state.harvesting}
+            onChange={this.handleChange}
+            placeholder='Harvesting'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='plantImage'>Image URL</Label>
+          <Input
+            type='url'
+            name='image'
+            value={this.state.image}
+            onChange={this.handleChange}
+            placeholder='Enter an Image URL or Upload a File'
+            className='form-control form-control-lg m-1'
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for='plantFile'>Upload Image</Label>
+          <Input
+            type='file'
+            name='filename'
+            id='myFile'
+            className='m-2'
+            accept='image/*'
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
--- src/views/Plants.js
+++ src/views/Plants.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { getAllPlants } from '../helpers/data/plantData';
 import PlantsCard from '../components/Cards/PlantsCard';
+import getUid from '../helpers/data/authData';
+import { getUsersGardens } from '../helpers/data/gardenData';
+import PlantsForm from '../components/Forms/PlantsForm';
+import MyModal from '../components/MyModal';
 
 export default class Plants extends Component {
   state = {
@@ -19,18 +23,28 @@ export default class Plants extends Component {
     });
   };
 
-  render() {
-    const { plants } = this.state;
-    const showPlants = () => (
-      plants.map((plant) => <PlantsCard key={plant.firebaseKey} plant={plant} />)
-    );
-    return (
+     getGardens = () => {
+       const currentUid = getUid();
+       getUsersGardens(currentUid).then((response) => {
+         this.setState({ gardens: response });
+       });
+     }
+
+     render() {
+       const { plants, gardens } = this.state;
+       const showPlants = () => (
+         plants.map((plant) => <PlantsCard key={plant.firebaseKey} plant={plant} />)
+       );
+       return (
       <div>
+        <MyModal title={'Add Plant'} buttonLabel={'Add Plant'}>
+          <PlantsForm plant={plants} gardens={gardens} onUpdate={this.getPlants}/>
+            </MyModal>
         <h1 className="text-white">Plants</h1>
         <div className='plants d-flex flex-wrap justify-content-center'>
           {showPlants()}
           </div>
       </div>
-    );
-  }
+       );
+     }
 }
