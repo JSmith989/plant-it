713027b8fafd89a6c9a4a2ee19272de2c475708f
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-12 14:42:03 -0600

    update complete

--- src/components/Cards/PlantDetailsCard.js
+++ src/components/Cards/PlantDetailsCard.js
@@ -4,7 +4,7 @@ export default function PlantDetailsCard({ plant }) {
   return (
     <div className='card m-2 border-dark col-md-6' >
         <h1 className='card-title'>{plant.name}</h1>
-      <img className='card-img-top' src={plant.image} alt='Card cap' />
+      <img className='card-img' src={plant.image} alt='Card cap' />
       <div className='card-body'>
             <p className='card-text'>Zone: {plant.zone}</p>
             <p className='card-text'>Germination: {plant.germination}</p>
