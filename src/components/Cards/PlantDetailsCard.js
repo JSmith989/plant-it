1e65bc6acdfb273828c25a45f1680af5d43ad10b
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-15 23:17:05 -0600

    styled plants card

--- src/components/Cards/PlantDetailsCard.js
+++ src/components/Cards/PlantDetailsCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function PlantDetailsCard({ plant }) {
   return (
-    <div className='card m-2 border-dark col-md-6' >
-        <h1 className='card-title'>{plant.name}</h1>
+    <div className='card plantDetails m-2 border-light col-md-6' >
+        <h1 className='card-title m-2'>{plant.name}</h1>
       <img className='card-img' src={plant.image} alt='Card cap' />
       <div className='card-body'>
             <p className='card-text'>Zone: {plant.zone}</p>
