e665d9f51c6fd1b0086cd462e12a315d6cb13015
Jordan <67443077+JSmith989@users.noreply.github.com>
2021-01-03 20:01:28 -0600

    map repsonsive

--- src/views/Home.js
+++ src/views/Home.js
@@ -11,11 +11,13 @@ export default function Home({ user }) {
   };
 
   return (
-    <div>
+    <div className="grid">
       <h1 className="homeTitle">Plant-It!</h1>
       {loadComponent()}
+      <p className="descript p-2 border border-white text-white">
+      Organize your backyard garden with this app that allows you to create your own garden and add plants. You can add notes and pictures of your plants and gardens. Pictured below is the hardiness zone map so you can figure out when to plant your seeds.</p>
 
-      <img className="homePageImg" src="https://user-images.githubusercontent.com/67443077/101262780-b15e2280-3706-11eb-8398-f076aeab7839.png" alt="usda map" width="1100" height="700"/>
+      <img className="homePageImg" src="https://user-images.githubusercontent.com/67443077/101262780-b15e2280-3706-11eb-8398-f076aeab7839.png" alt="usda map" />
 
     </div>
   );
